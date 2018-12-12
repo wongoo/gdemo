ab953c8ec2fdb1185147613e36eeaf778d4327a4
wangoo <wangoo@soundbus.cn>
2018-12-12 11:23:22 +0800

    reflect value set

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -46,3 +46,43 @@ func TestType(t *testing.T) {
 	assert.Equal(t, ap, ap2)
 
 }
+
+func TestValueSet(t *testing.T) {
+	type B struct {
+		s string
+		R int
+	}
+
+	b := B{s: "x", R: 1}
+	bp := &b
+
+	bType := reflect.TypeOf(b)
+	bpType := reflect.TypeOf(bp)
+
+	bValue := reflect.ValueOf(b)
+	bpValue := reflect.ValueOf(bp)
+
+	t.Log("b:", b)
+	t.Log("b type:", bType)
+	t.Log("b value:", bValue)
+	t.Log("settability of b value:", bValue.CanSet())
+	t.Log("bp:", bp)
+	t.Log("bp type:", bpType)
+	t.Log("bp value(1):", bpValue)
+	t.Log("settability of bp value:", bpValue.CanSet())
+	t.Log("settability of bp elem value:", bpValue.Elem().CanSet())
+
+	b2 := B{s: "y", R: 2}
+
+	bpValue.Elem().Set(reflect.ValueOf(b2))
+	t.Log("bp value(2):", bpValue)
+
+	newBPtrValue := reflect.New(bType)
+	newBValue := newBPtrValue.Elem()
+	//newBValue.FieldByName("s").SetString("z") // reflect.Value.SetString using value obtained using unexported field
+	newBValue.FieldByName("R").SetInt(3)
+	bpValue.Elem().Set(newBValue)
+	t.Log("bp value(3):", bpValue)
+
+	// bpValue.Elem().Set(reflect.New(bpType).Elem().Elem()) // panic: reflect: call of reflect.Value.Set on zero Value<Paste>
+}
