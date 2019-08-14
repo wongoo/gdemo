efbffdd220e49e1598d1d2e98b963ecacff99094
wongoo <gelnyang@163.com>
2019-08-14 09:32:34 +0800

    slice copy

--- basic/slice_test.go
+++ basic/slice_test.go
@@ -1,6 +1,7 @@
 package basic
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
@@ -37,4 +38,39 @@ func TestSliceAppendNil(t *testing.T) {
 	assert.Equal(t, "a", list2[0].Text)
 	assert.Nil(t, list2[1])
 	assert.Equal(t, "c", list2[2].Text)
+}
+
+func TestSliceOutOfIndex(t *testing.T) {
+	s := make([]int, 3, 3)
+	fmt.Println(s[3:]) // nil slice
+	fmt.Println(s[4:]) // slice bounds out of range
+}
+
+type sscc struct {
+	s string
+}
+
+func TestSliceCopyNotOverwrite(t *testing.T) {
+	e := make([]sscc, 3)
+	e[0].s = "a"
+	e[1].s = "b"
+	e[2].s = "c"
+
+	c1 := e[0]
+	assert.Equal(t, "a", c1.s)
+	copy(e[0:], e[1:])
+	assert.Equal(t, "a", c1.s)
+	assert.Equal(t, "b", e[0].s)
+
+	a := make([]*sscc, 3)
+	a[0] = &sscc{s: "1"}
+	a[1] = &sscc{s: "2"}
+	a[2] = &sscc{s: "3"}
+
+	c2 := a[0]
+	assert.Equal(t, "1", c2.s)
+	copy(a[0:], a[1:])
+	assert.Equal(t, "1", c2.s)
+	assert.Equal(t, "2", a[0].s)
+
 }
